b13c36de34ef40ec5b1653e8a675d5528c2e43d1
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-01-11 17:19:53 +0200

    Add loading

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,32 @@ import './App.css';
 
 const App = () => {
   const [foods, setFoods] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     (async () => {
+      setLoading(true);
       const res = await axios.get(
         `${process.env.REACT_APP_API_URL}/api/food/today`
       );
       setFoods(res.data.foods);
+      setLoading(false);
     })();
   }, []);
 
   return (
     <div className="center">
-      {foods.map((food) => (
-        <h1 key={v4()} className="text">
-          {food}
-        </h1>
-      ))}
+      {loading ? (
+        <h1 className="text">loading...</h1>
+      ) : (
+        <>
+          {foods.map((food) => (
+            <h1 key={v4()} className="text">
+              {food}
+            </h1>
+          ))}
+        </>
+      )}
     </div>
   );
 };
