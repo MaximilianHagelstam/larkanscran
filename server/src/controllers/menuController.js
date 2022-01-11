47869245aad4e1c85d5318f21a3d77c3c77e65d8
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-01-11 10:29:31 +0200

    Get lärkan api from backend

--- server/src/controllers/menuController.js
+++ server/src/controllers/menuController.js
@@ -1,10 +1,18 @@
-const getToday = (_req, res) => {
+const axios = require('axios');
+
+const getToday = async (_req, res) => {
   const date = new Date();
+
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
 
   console.log(year, month, day);
+
+  const { data } = await axios.get(
+    'https://larkan.skolkalender.fi/kalender/Events?start=2022-01-11&end=2022-01-12'
+  );
+  console.log(data);
 
   res.json({ message: 'Hello team!' });
 };
