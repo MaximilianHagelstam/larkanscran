c579eaf5a33b250bd8cbd123b39660d97864a190
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-01-11 10:02:47 +0200

    connect backend to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,10 @@ const App = () => {
     const month = date.getMonth() + 1;
     const day = date.getDate();
 
+    fetch(`${process.env.REACT_APP_API_URL}/api/menu/today`)
+      .then(res => res.json())
+      .then(data => console.log(data));
+
     console.log(year, month, day);
   }, [date]);
 
--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const menuRoutes = require('./routes/menuRoutes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/menu', menuRoutes);
 
