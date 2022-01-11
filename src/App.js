fba5ef67c382f3d4a51efb41ae93a121ca298f96
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-01-11 08:42:07 +0200

    Get current date

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import { useEffect, useState } from 'react';
+
 const App = () => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+   const year = date.getFullYear();
+   const month = date.getMonth() + 1;
+   const day = date.getDate();
+
+   console.log(year, month, day)
+  }, [date]);
+
   return <p>James</p>;
 };
 
