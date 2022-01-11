e6709e9b98952312de49eb73d50a47cc77b248ba
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-01-11 10:25:40 +0200

    Add date logic to backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 const App = () => {
-  const [date, setDate] = useState(new Date());
-
   useEffect(() => {
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-
     fetch(`${process.env.REACT_APP_API_URL}/api/menu/today`)
       .then(res => res.json())
       .then(data => console.log(data));
-
-    console.log(year, month, day);
-  }, [date]);
+  }, []);
 
   return <p>James</p>;
 };
--- server/src/controllers/menuController.js
+++ server/src/controllers/menuController.js
@@ -1,4 +1,11 @@
 const getToday = (_req, res) => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  console.log(year, month, day);
+
   res.json({ message: 'Hello team!' });
 };
 
